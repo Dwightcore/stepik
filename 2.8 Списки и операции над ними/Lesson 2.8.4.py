2fba8dafd1aa75eda3274e1b0c5990b7961bed0a
Mikhail Sokolov <mikhail.sokolov@simbirsoft.com>
2022-06-09 20:26:15 +0400

    F-strings

---
+++ 2.8 Списки и операции над ними/Lesson 2.8.4.py
@@ -0,0 +1,2 @@
+mas = list(map(int, input().split()))
+print(f"{min(mas)} {max(mas)}")
---
+++ F - strings/Lesson 2.7.3.py
@@ -0,0 +1,3 @@
+name = input().title()
+age = int(input())
+print(f"{name}, вам исполнится 77 лет в {age + 77}")
